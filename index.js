6c253bb4fbe0fdae1ae6884989e604db5737d041
JacobWestfield <91150870+JacobWestfield@users.noreply.github.com>
2021-09-22 00:38:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Input from './Input.jsx';
+
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Input />
+    {/* <Output data={messages} /> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
